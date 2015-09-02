8d492c14fbdc9c908230295d488080c074f1c1bd
Yao <zhouyao@Mac-mini.local>
2015-09-02 18:24:58 +0800

    DockItem update

--- ZYSinaMicroBlog/ZYSinaMicroBlog/Classes/Other/ZYNavigationController.m
+++ ZYSinaMicroBlog/ZYSinaMicroBlog/Classes/Other/ZYNavigationController.m
@@ -16,22 +16,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UINavigationBar *bar = [UINavigationBar appearance];
+    //
+    [bar setBackgroundImage:[UIImage imageNamed:@"navigationbar_background.png"] forBarMetrics:UIBarMetricsDefault];
+    NSShadow * shardow = [[NSShadow alloc]init];
+    [shardow setShadowOffset:CGSizeZero];
+    [bar setTitleTextAttributes:@{
+                                  NSForegroundColorAttributeName:[UIColor grayColor],
+                                  NSShadowAttributeName:shardow
+                                  } ];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
