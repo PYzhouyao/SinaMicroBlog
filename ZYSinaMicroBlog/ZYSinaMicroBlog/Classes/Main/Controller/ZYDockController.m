af40c6adae0148903e59d9e4c9fa7af4ce8876b3
Yao <zhouyao@Mac-mini.local>
2015-09-02 15:29:34 +0800

    添加一些注释

--- ZYSinaMicroBlog/ZYSinaMicroBlog/Classes/Main/Controller/ZYDockController.m
+++ ZYSinaMicroBlog/ZYSinaMicroBlog/Classes/Main/Controller/ZYDockController.m
@@ -19,19 +19,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self addDock];
 }
 
+#pragma mark 添加Dock控件到当前控制器
 -(void)addDock{
     self.dock = [[ZYDock alloc]init];
+    //设置Dock的代理为当前的控制器
     self.dock.delegate = self;
     
+    // 将Dock添加到当前控制器的View上面，即调整Dock尺寸位置让其显示到屏幕最底部
     self.dock.frame = CGRectMake(0, self.view.frame.size.height - kDockHeight, self.view.frame.size.width, kDockHeight);
-    
     [self.view addSubview:self.dock];
     
 }
 
--(void)dock:(ZYDock *)dock itemSelectFrol:(NSInteger)sourceIndex to:(NSInteger)toIndex;{
+#pragma mark 首页控制器上View的切换
+// Dock的代理方法，当Dock上按钮被点击时，切换相应的View到当前控制器上显示
+-(void)dock:(ZYDock *)dock itemSelectFrom:(NSInteger)sourceIndex to:(NSInteger)toIndex;{
     
     if (toIndex < 0 || toIndex >= self.childViewControllers.count) return;
     
--- ZYSinaMicroBlog/ZYSinaMicroBlog/Classes/Main/View/ZYDock.m
+++ ZYSinaMicroBlog/ZYSinaMicroBlog/Classes/Main/View/ZYDock.m
@@ -23,21 +23,23 @@
     return self;
 }
 
+#pragma mark Dock上添加按钮
 -(void)addItemWithIcon:(NSString *)iconName selectedIcon:(NSString *)selectedIconName title:(NSString *)title{
     ZYDockItem *item = [[ZYDockItem alloc]init];
     [self addSubview:item];
     
-    [item setTitle:title forState:UIControlStateNormal];
-    [item setImage:[UIImage imageNamed:iconName] forState:UIControlStateNormal];
+    [item setTitle:title forState:UIControlStateNormal];// 设置按钮文字
+    [item setImage:[UIImage imageNamed:iconName] forState:UIControlStateNormal];// 设置按钮不同状态下的图片
     [item setImage:[UIImage imageNamed:selectedIconName] forState:UIControlStateSelected];
     
-    [item addTarget:self action:@selector(itemEvent:) forControlEvents:UIControlEventTouchDown];
+    [item addTarget:self action:@selector(itemEvent:) forControlEvents:UIControlEventTouchDown];//按钮响应事件
     
     
     NSUInteger count = self.subviews.count;
     CGFloat width = self.frame.size.width /count;
     CGFloat hight = self.frame.size.height;
     
+    //根据按钮个数动态设置按钮尺寸位置
     for (int i = 0; i < count; i++) {
         ZYDockItem *item = self.subviews[i];
         item.tag = i ;
@@ -45,22 +47,25 @@
     }
     
     if (nil == self.SelectedItem) {
-        [self itemEvent:self.subviews[0]];
+        [self itemEvent:self.subviews[0]]; //默认第一个按钮为点中状态
     }
 
 }
 
+#pragma mark 被选状态切换
+// 点击当前按钮，则当前按钮置于被选择状态，其他按钮置于未被选择状态
 -(void)itemEvent:(ZYDockItem *)item{
     
-    //切换按钮状态
+    // 0、通知代理
     if ([self.delegate respondsToSelector:@selector(dock:itemSelectFrol:to:)]) {
         [self.delegate dock:self itemSelectFrol:(NSInteger)self.SelectedItem.tag to:(NSInteger)item.tag];
     }
     
+    // 1、取消之前按钮的select状态
     self.SelectedItem.selected = NO;
-    
+    // 2、设置当前按钮select状态
     item.selected = YES;
-    
+    // 3、重新赋值给成员变量
     self.SelectedItem = item;
 }
 @end
